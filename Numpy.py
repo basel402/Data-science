2f58bfa571fda01cd0f78b0448bc08c8e4e399cc
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-08-28 22:21:09 +0300

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -61,4 +61,50 @@ arr = np.concatenate((arr1, arr2), axis=1)
 print(arr) # prints [[1 2 5 6] [3 4 7 8]]
 
 
+# array search
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+x = np.where(arr%2 == 0)
+print(x) # print array of indices of even elements in the arr
+
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 7)
+print(x) # print the index of first element >= parameter sent , you can send array instead of one element and it will return array of indices
+
+
+# array sort 
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+print(np.sort(arr)) # it sorts every array insde the 2d array , u can use function sort normally with 1d arrays
+
+
+# array filtering
+arr = np.array([41, 42, 43, 44])
+x = [True, False, True, False]
+newarr = arr[x]
+print(newarr) # every true element is put in the array and the false is execluded
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+filter_arr = arr % 2 == 0
+newarr = arr[filter_arr]
+print(filter_arr)
+print(newarr) # prints even elements in the arr only
+
+
+
+# random in numpy, you can put "size" parameter to determine the shape of x -> 2d array on anything
+x=random.randint(100) # random num from 0 -> 100
+x = random.rand() # random float num from 0 -> 1 
+x = random.choice([3, 5, 7, 9]) # random num in this array
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100)) # defines 1d array of size 100 and probability of each number to occur in the answer
+random.shuffle(arr) # shuffles the array inplace
+random.permutation(arr) # shuffle the array not inplace
+
+
+
+
+
+
+
+
+
+
 
